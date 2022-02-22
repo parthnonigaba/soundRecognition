117527254f07c9084e7c7c038aab20ec0102ed63
parthnonigaba <85085655+parthnonigaba@users.noreply.github.com>
2022-02-22 11:42:51 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function startClassification()
+function startClassifacation()
 {
   navigator.mediaDevices.getUserMedia({ audio: true, video:false});
-  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', { probabilityThreshold: 0.7 } ,modelReady);
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/i-s_uWLkg/', { probabilityThreshold: 0.7 } ,modelReady);
 }
 
 function modelReady(){
